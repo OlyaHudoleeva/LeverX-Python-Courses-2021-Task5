f78875cbf7c391e9b96d73a3cbcbdbcbb76af7a0
Volha Khudaleyeva <olga.hudoleeva98@gmail.com>
2021-06-04 12:18:29 +0300

    [feature] create/read  comments and rates

--- online-courses/online_courses/lesson/router.py
+++
@@ -1,6 +0,0 @@
-# from rest_framework import routers
-#
-# from lesson.views import CourseViewSet
-#
-# router = routers.DefaultRouter()
-# router.register('courses', CourseViewSet, basename='courses')
--- online-courses/online_courses/lesson/urls.py
+++ online-courses/online_courses/lesson/urls.py
@@ -5,21 +5,23 @@ from lesson import views
 # from lesson.router import router
 
 urlpatterns = [
-    # path('', include(router.urls)),
     path('auth/registration', views.UserRegistration.as_view()),
     path('auth/', include('djoser.urls.authtoken')),
     path('courses/', views.CourseList.as_view()),
     path('courses/<int:pk>', views.CourseDetail.as_view()),
-    path('add/user/course/<int:pk>', views.UserToCourseAddition.as_view()),
-    path('delete/user/course/<int:pk>', views.UserFromCourseRemover.as_view()),
-    path('student/courses/list', views.StudentCoursesList.as_view()),
-    path('course/lectures/', views.LectureList.as_view()),
-    path('course/lectures/<int:pk>', views.LectureDetail.as_view()),
-    path('user/course/<int:pk>/lectures/list/', views.CourseLecturesList.as_view()),
-    path('course/lecture/homework/add', views.HomeworkToLectureAddition.as_view()),
-    path('user/course/lecture/<int:pk>/homeworks/list', views.LectureHomeworksList.as_view()),
-    path('student/homeworks/list', views.StudentHomeworksList.as_view()),
-    path('student/homework/complete', views.StudentHomeworkFinishing.as_view()),
-    path('homeworks/completed/list', views.CompletedStudentsHomeworksList.as_view())
-
+    path('courses/<int:pk>/user/add', views.UserToCourseAddition.as_view()),
+    path('courses/<int:pk>/user/delete', views.UserFromCourseRemover.as_view()),
+    path('student/courses', views.StudentCoursesList.as_view()),
+    path('lectures/', views.LectureList.as_view()),
+    path('lectures/<int:pk>', views.LectureDetail.as_view()),
+    path('courses/<int:pk>/lectures', views.CourseLecturesList.as_view()),
+    path('homeworks/add', views.HomeworkToLectureAddition.as_view()),
+    path('lectures/<int:pk>/homeworks', views.LectureHomeworksList.as_view()),
+    path('student/homeworks', views.StudentHomeworksList.as_view()),
+    path('student/homeworks/complete', views.StudentHomeworkFinishing.as_view()),
+    path('homeworks/completed', views.CompletedStudentsHomeworksList.as_view()),
+    path('homeworks/rates/add', views.RateOrCommentToStudentHomeworkAddition.as_view()),
+    path('homeworks/teacher-comment/add', views.RateOrCommentToStudentHomeworkAddition.as_view()),
+    path('homeworks/student-comment/add', views.StudentCommentToRateAddition.as_view()),
+    path('homeworks/rates', views.HomeworkRatesList.as_view())
 ]
